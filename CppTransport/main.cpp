53b4afe8b45f68e3f00ef4c83c952c7ea63a03e2
ds283 <D.Seery@sussex.ac.uk>
2013-06-12 15:01:59 +0100

    Initial commit

---
+++ CppTransport/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CppTransport
+//
+//  Created by David Seery on 12/06/2013.
+//  Copyright (c) 2013 University of Sussex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char *argv[])
+  {
+    std::cout << "CppTransport (c) David Seery and others 2013";
+    return 0;
+  }
+
