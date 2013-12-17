0d6e83d2d94a701ace72cd4ac113dac20598fe9f
ds283 <D.Seery@sussex.ac.uk>
2013-12-17 19:33:49 +0000

    Add double-summation macros for coordinates, fields and parameters (cherry picked from commit 6e9f943)

--- CppTransport/backends/shared/summation.cpp
+++ CppTransport/backends/shared/summation.cpp
@@ -42,31 +42,38 @@ namespace macro_packages
         std::vector<macro_packages::index_rule> package;
 
         const std::vector<replacement_rule_pre> pres =
-                                                  { nullptr, nullptr, nullptr
+                                                  { nullptr, nullptr, nullptr,
+                                                    nullptr, nullptr, nullptr
                                                   };
 
         const std::vector<replacement_rule_post> posts =
-                                                   { nullptr, nullptr, nullptr
+                                                   { nullptr, nullptr, nullptr,
+                                                     nullptr, nullptr, nullptr
                                                    };
 
         const std::vector<replacement_rule_index> rules =
-                                                    { BIND3(null), BIND3(null), BIND3(null)
+                                                    { BIND3(null), BIND3(null), BIND3(null),
+                                                      BIND3(null), BIND3(null), BIND3(null)
                                                     };
 
         const std::vector<std::string> names =
-                                         { "SUM_COORDS", "SUM_FIELDS", "SUM_PARAMS"
+                                         { "SUM_COORDS", "SUM_FIELDS", "SUM_PARAMS",
+                                           "SUM_2COORDS", "SUM_2FIELDS", "SUM_2PARAMS",
                                          };
 
         const std::vector<unsigned int> args =
-                                          { 0, 0, 0
+                                          { 0, 0, 0,
+                                            0, 0, 0
                                           };
 
         const std::vector<unsigned int> indices =
-                                          { 1, 1, 1
+                                          { 1, 1, 1,
+                                            2, 2, 2
                                           };
 
         const std::vector<unsigned int> ranges =
-                                          { 2, 1, INDEX_RANGE_PARAMETER
+                                          { 2, 1, INDEX_RANGE_PARAMETER,
+                                            2, 1, INDEX_RANGE_PARAMETER
                                           };
 
         assert(pres.size() == posts.size());
