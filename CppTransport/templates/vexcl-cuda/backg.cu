dc5bcfa86f3115df86a1bc1681bd013cbee99ac6
ds283 <D.Seery@sussex.ac.uk>
2013-12-17 21:04:03 +0000

    Switch VexCL CUDA kernels to use a shared memory strategy

--- CppTransport/templates/vexcl-cuda/backg.cu
+++ CppTransport/templates/vexcl-cuda/backg.cu
@@ -5,25 +5,25 @@ typedef unsigned int        uint;
 typedef unsigned short      ushort;
 typedef unsigned long long  ulong;
 
-extern "C" __global__ void backg( ulong n, $$__PARAM_ARGS, $$__COORD_ARGS{__x}, $$__COORD_ARGS{__dxdt}, double __Mp )
+extern "C" __global__ void backg( ulong n,
+                                  double __Mp,
+                                  $$__PARAM_ARGS,
+                                  $$__COORD_ARGS{__x},
+                                  $$__COORD_ARGS{__dxdt} )
   {
     size_t begin = blockDim.x * blockIdx.x + threadIdx.x;
 
     if(begin < n)
       {
-        #undef  $$__COORDINATE[A]
         #define $$__COORDINATE[A] __x_$$__A[begin]
 
         double __Hsq = $$__HUBBLE_SQ;
         double __eps = $$__EPSILON;
 
-        #undef  $$__COORDINATE[A]
-        #undef  __k
-        #define $$__COORDINATE[A] __x_$$__A[idx]
+        $$__TEMP_POOL{"double $1 = $2;"}
 
         for(size_t idx = begin, grid_size = blockDim.x * gridDim.x; idx < n; idx += grid_size)
           {
-            $$__TEMP_POOL{"double $1 = $2;"}
             __dxdt_$$__A[idx] = $$__U1_PREDEF[A]{__Hsq, __eps};
           }
       }
--- CppTransport/templates/vexcl-cuda/twopf.cu
+++ CppTransport/templates/vexcl-cuda/twopf.cu
@@ -5,11 +5,73 @@ typedef unsigned int        uint;
 typedef unsigned short      ushort;
 typedef unsigned long long  ulong;
 
-extern "C" __global__ void twopffused( ulong n, $$__TWOPF_ARGS{__twopf}, $$__TWOPF_ARGS{__dtwopf}, $$__U2_ARGS{__u2} )
+extern "C" __global__ void twopffused( ulong n,
+                                       double __Mp,
+                                       $$__PARAM_ARGS,
+                                       $$__COORD_ARGS{__x},
+                                       double* __klist,
+                                       double __a,
+                                       $$__TWOPF_ARGS{__twopf},
+                                       $$__COORD_ARGS{__dxdt},
+                                       $$__TWOPF_ARGS{__dtwopf} )
   {
-    for(size_t idx = blockDim.x * blockIdx.x + threadIdx.x, grid_size = blockDim.x * gridDim.x; idx < n; idx += grid_size)
+    size_t begin = blockDim.x * blockIdx.x + threadIdx.x;
+
+    #define __U2_SIZE    (2*$$__NUMBER_FIELDS*2*$$__NUMBER_FIELDS)
+    #define __TWOPF_SIZE (2*$$__NUMBER_FIELDS*2*$$__NUMBER_FIELDS)
+
+    // SPACE NEEDED IN SHARED MEMORY:
+    // k-modes: 1 per thread in the block   = sizeof(double)*blockDim.x
+    //                                      = 8*blockDim.x bytes
+    // u2-tensor: 1 per thread in the block = __U2_SIZE*blockDim.x*sizeof(double)
+    //                                      = 16*8*blockDim.x bytes = 128*blockDim.x bytes
+    // twopf: 1 per thread in the block     = __TWOPF_SIZE*blockDim.x*sizeof(double)
+    //                                      = 16*8*blockDim.x bytes = 128*blockDim.x bytes
+
+    // TOTAL                                = (8+128+128)*blockDim.x bytes = 264*blockDim.x bytes
+    extern double __shared__ smem[];
+
+    // index into k-modes
+    #define __k smem[threadIdx.x]
+
+    // index into u2 tensor
+    // we allocate all u2 tensors in a block after the k-modes, so have to offset by blockDim.x
+    #define __U2(a,b) smem[blockDim.x + threadIdx.x*__U2_SIZE + 2*$$__NUMBER_FIELDS*a + b]
+
+    // index into twopf
+    // twopfs come in a block after the u2-tensors, so we have to offset by blockDim.x + __U2_SIZE*blockDim.x
+    #define __TWOPF(a,b) smem[blockDim.x + __U2_SIZE*blockDim.x + threadIdx.x*__TWOPF_SIZE + 2*$$__NUMBER_FIELDS*a + b]
+
+    if(begin < n)
       {
-        __dtwopf_$$__A_$$__B[idx]  = 0 $$// + $$__U2_NAME[AC]{__u2}[idx] * __twopf_$$__C_$$__B[idx];
-        __dtwopf_$$__A_$$__B[idx] += 0 $$// + $$__U2_NAME[BC]{__u2}[idx] * __twopf_$$__A_$$__C[idx];
+        size_t grid_size = blockDim.x * gridDim.x;  // number of threads in the grid = stride size per kernel
+
+        // read coords into local variables
+        // we only need one, which we can choose to be 'begin'
+        double $$__COORDINATE[A] = __x_$$__A[begin];
+
+        double __Hsq = $$__HUBBLE_SQ;
+        double __eps = $$__EPSILON;
+
+        // cache u1 tensors for evolution of the background
+        $$__TEMP_POOL{"double $1 = $2;"}
+        double __u1_$$__A = $$__U1_PREDEF[A]{__Hsq, __eps};
+
+        for(size_t idx = begin; idx < n; idx += grid_size)
+          {
+            // copy the data we need from global memory into shared memory
+            __k = __klist[idx];
+            __TWOPF($$__A, $$__B) = __twopf_$$__A_$$__B[idx]; $$//
+
+            // compute u2 for the k-mode we are looking at
+            $$__TEMP_POOL{"double $1 = $2;"}
+
+            __U2($$__A,$$__B) = $$__U2_PREDEF[AB]{__k, __a, __Hsq, __eps};
+
+            __dxdt_$$__A[idx] = __u1_$$__A; $$//
+
+            __dtwopf_$$__A_$$__B[idx]  = 0 $$// + $$__SUM_COORDS[C] __U2($$__A,$$__C) * __TWOPF($$__C,$$__B);
+            __dtwopf_$$__A_$$__B[idx] += 0 $$// + $$__SUM_COORDS[C] __U2($$__B,$$__C) * __TWOPF($$__A,$$__C);
+          }
       }
   }
