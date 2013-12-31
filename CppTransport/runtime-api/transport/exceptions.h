e785219d17f33d40b081820d4ba33c94800f3d9d
ds283 <D.Seery@sussex.ac.uk>
2013-12-31 22:19:28 +0000

    Add 'runtime_exception' to implement internal exception handling with error codes

---
+++ CppTransport/runtime-api/transport/exceptions.h
@@ -0,0 +1,41 @@
+//
+// Created by David Seery on 31/12/2013.
+// Copyright (c) 2013 University of Sussex. All rights reserved.
+//
+
+
+#ifndef __exceptions_H_
+#define __exceptions_H_
+
+
+#include <string>
+#include <stdexcept>
+
+
+namespace transport
+  {
+
+    class runtime_exception: public std::runtime_error
+      {
+        // CONSTRUCTOR, DESTRUCTOR
+
+      public:
+
+        typedef enum { RUNTIME_ERROR, MPI_ERROR, TASK_NOT_FOUND } exception_type;
+        runtime_exception(exception_type t, const std::string msg)
+          : type(t), std::runtime_error(msg)
+          {
+          }
+
+        exception_type get_exception_code() { return(this->type); }
+
+        // INTERNAL DATA
+      protected:
+        exception_type type;
+      };
+
+  }   // namespace transport
+
+
+
+#endif //__exceptions_H_
