01872ebb0716773e6d3ccd1a99a208ec8e99142c
ds283 <D.Seery@sussex.ac.uk>
2014-01-07 17:02:52 +0000

    Add 'version.h' header containing runtime API version

---
+++ CppTransport/runtime-api/transport/version.h
@@ -0,0 +1,14 @@
+//
+// Created by David Seery on 07/01/2014.
+// Copyright (c) 2014 University of Sussex. All rights reserved.
+//
+
+
+#ifndef __version_H_
+#define __version_H_
+
+
+#define __CPP_TRANSPORT_RUNTIME_API_VERSION (007)
+
+
+#endif //__version_H_
