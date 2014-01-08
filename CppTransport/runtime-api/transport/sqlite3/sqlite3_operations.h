53bbd5d648d0912fd107eef1cd662d246fbc3835
ds283 <D.Seery@sussex.ac.uk>
2014-01-08 22:44:14 +0000

    Implement population of twopf- and threepf-sample tables in the SQL container

--- CppTransport/runtime-api/transport/sqlite3/sqlite3_operations.h
+++ CppTransport/runtime-api/transport/sqlite3/sqlite3_operations.h
@@ -102,16 +102,44 @@ namespace transport
                 msg << __CPP_TRANSPORT_DATACTR_TWOPFTAB_FAIL << errmsg << ")";
                 throw runtime_exception(runtime_exception::DATA_CONTAINER_ERROR, msg.str());
               }
+
+            std::stringstream insert_stmt;
+            insert_stmt << "INSERT INTO twopf_samples VALUES (@serial, @conventional, @comoving)";
+
+            sqlite3_stmt* stmt;
+            sqlite3_prepare_v2(db, insert_stmt.str().c_str(), insert_stmt.str().length()+1, &stmt, nullptr);
+
+            sqlite3_exec(db, "BEGIN TRANSACTION", nullptr, nullptr, &errmsg);
+
+            for(unsigned int i = 0; i < conventional_list.size(); i++)
+              {
+                sqlite3_bind_int(stmt, 1, i);
+                sqlite3_bind_double(stmt, 2, conventional_list[i]);
+                sqlite3_bind_double(stmt, 3, comoving_list[i]);
+
+                status = sqlite3_step(stmt);
+                if(status != SQLITE_DONE)
+                  {
+                    std::ostringstream msg;
+                    msg << __CPP_TRANSPORT_DATACTR_TWOPFTAB_FAIL << sqlite3_errmsg(db) << ")";
+                    throw runtime_exception(runtime_exception::DATA_CONTAINER_ERROR, msg.str());
+                  }
+
+                sqlite3_clear_bindings(stmt);
+                sqlite3_reset(stmt);
+              }
+
+            sqlite3_exec(db, "END TRANSACTION", nullptr, nullptr, &errmsg);
           }
 
 
         template <typename number>
-        void create_threepf_sample_table(sqlite3* db, task<number>* tk)
+        void create_threepf_sample_table(sqlite3* db, threepf_task<number>* tk)
           {
             assert(db != nullptr);
             assert(tk != nullptr);
 
-            std::vector<double> sample_times = tk->get_sample_times();
+            std::vector<threepf_kconfig> threepf_sample = tk->get_sample();
 
             std::stringstream stmt_text;
             stmt_text << "CREATE TABLE threepf_samples("
@@ -119,6 +147,9 @@ namespace transport
               << "wavenumber1 INTEGER,"
               << "wavenumber2 INTEGER,"
               << "wavenumber3 INTEGER,"
+              << "kt_com      DOUBLE,"
+              << "alpha       DOUBLE,"
+              << "beta        DOUBLE,"
               << "FOREIGN KEY(wavenumber1) REFERENCES twopf_samples(serial),"
               << "FOREIGN KEY(wavenumber2) REFERENCES twopf_samples(serial),"
               << "FOREIGN KEY(wavenumber3) REFERENCES twopf_samples(serial)"
@@ -133,6 +164,38 @@ namespace transport
                 msg << __CPP_TRANSPORT_DATACTR_THREEPFTAB_FAIL << errmsg << ")";
                 throw runtime_exception(runtime_exception::DATA_CONTAINER_ERROR, msg.str());
               }
+
+            std::stringstream insert_stmt;
+            insert_stmt << "INSERT INTO threepf_samples VALUES (@serial, @wn1, @wn2, @wn3, @kt, @alpha, @beta)";
+
+            sqlite3_stmt* stmt;
+            sqlite3_prepare_v2(db, insert_stmt.str().c_str(), insert_stmt.str().length()+1, &stmt, nullptr);
+
+            sqlite3_exec(db, "BEGIN TRANSACTION", nullptr, nullptr, &errmsg);
+
+            for(unsigned int i = 0; i < threepf_sample.size(); i++)
+              {
+                sqlite3_bind_int(stmt, 1, i);
+                sqlite3_bind_int(stmt, 2, threepf_sample[i].index[0]);
+                sqlite3_bind_int(stmt, 3, threepf_sample[i].index[1]);
+                sqlite3_bind_int(stmt, 4, threepf_sample[i].index[2]);
+                sqlite3_bind_double(stmt, 5, threepf_sample[i].k_t);
+                sqlite3_bind_double(stmt, 6, threepf_sample[i].alpha);
+                sqlite3_bind_double(stmt, 7, threepf_sample[i].beta);
+
+                status = sqlite3_step(stmt);
+                if(status != SQLITE_DONE)
+                  {
+                    std::ostringstream msg;
+                    msg << __CPP_TRANSPORT_DATACTR_THREEPFTAB_FAIL << sqlite3_errmsg(db) << ")";
+                    throw runtime_exception(runtime_exception::DATA_CONTAINER_ERROR, msg.str());
+                  }
+
+                sqlite3_clear_bindings(stmt);
+                sqlite3_reset(stmt);
+              }
+
+            sqlite3_exec(db, "END TRANSACTION", nullptr, nullptr, &errmsg);
           }
 
       }   // namespace sqlite3_operations
