4e3ff47be737b3fc1b1ec767e9fe311cbe201de3
ds283 <D.Seery@sussex.ac.uk>
2014-05-04 17:17:27 +0100

    Begin implementation of unqlite-based repository

---
+++ CppTransport/runtime-api/transport/unqlite/repository_unqlite.h
@@ -0,0 +1,17 @@
+//
+// Created by David Seery on 04/05/2014.
+// Copyright (c) 2014 University of Sussex. All rights reserved.
+//
+
+
+#ifndef __repository_unqlite_H_
+#define __repository_unqlite_H_
+
+
+class repository_unqlite
+	{
+
+	};
+
+
+#endif //__repository_unqlite_H_
