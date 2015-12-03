8432fb6baaec363cfa495a6c32ab8fd9baa44317
ds283 <D.Seery@sussex.ac.uk>
2015-12-03 11:52:07 +0000

    Add test target for runtime API
    - allows an in-source-tree build using the source tree headers, which makes development more straightforward

---
+++ CppTransport/test-target/test_target.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp (axion-mpi)
+//  axion
+//
+//  Created by David Seery on 26/06/2013.
+//  Copyright (c) 2013-15 University of Sussex. All rights reserved.
+//
+
+
+#include "axion_basic_unrolled.h"
+
+
+// ****************************************************************************
+
+
+int main(int argc, char* argv[])
+  {
+    // set up an instance of a manager
+    std::unique_ptr< transport::task_manager<double> > mgr = std::make_unique< transport::task_manager<double> >(argc, argv);
+
+    // set up an instance of the double quadratic model,
+    // using doubles, with given parameter choices
+    std::shared_ptr< transport::axion_basic<double> > model = mgr->create_model< transport::axion_basic<double> >();
+
+    mgr->process();
+
+    return(EXIT_SUCCESS);
+  }
