7c8893553a789190552992b583ba8bcf304c4e61
David Seery <D.Seery@sussex.ac.uk>
2015-12-21 03:40:51 +0000

    More efficient implementation of to_momentum, to_species methods in index_traits

--- CppTransport/transport-objects/indices/index_traits.cpp
+++ CppTransport/transport-objects/indices/index_traits.cpp
@@ -16,7 +16,7 @@ bool index_traits::is_species(phase_index index)
 bool index_traits::is_momentum(phase_index index)
   {
     unsigned raw = static_cast<unsigned int>(index);
-    return(raw >= this->num_fields && raw < 2* this->num_fields);
+    return(raw >= this->num_fields && raw < 2 * this->num_fields);
   }
 
 
@@ -30,12 +30,14 @@ phase_index index_traits::to_momentum(field_index index)
 phase_index index_traits::to_momentum(phase_index index)
   {
     unsigned raw = static_cast<unsigned int>(index);
-    return phase_index((raw % this->num_fields) + this->num_fields);
+    if(raw < this->num_fields) raw += this->num_fields;
+    return phase_index(raw);
   }
 
 
 field_index index_traits::to_species(phase_index index)
   {
     unsigned raw = static_cast<unsigned int>(index);
-    return field_index(raw % this->num_fields);
+    if(raw >= this->num_fields) raw -= this->num_fields;
+    return field_index(raw);
   }
