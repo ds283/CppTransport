e14f865ceb0dc26fc083099f51a5a3e06b6c6953
ds283 <D.Seery@sussex.ac.uk>
2015-12-22 18:26:29 +0000

    Add skeleton file for lambda-management class

---
+++ CppTransport/backends/infrastructure/language_concepts/lambdas.h
@@ -0,0 +1,16 @@
+//
+// Created by David Seery on 22/12/2015.
+// Copyright (c) 2015 University of Sussex. All rights reserved.
+//
+
+#ifndef CPPTRANSPORT_LAMBDAS_H
+#define CPPTRANSPORT_LAMBDAS_H
+
+
+class lambdas
+  {
+
+  };
+
+
+#endif //CPPTRANSPORT_LAMBDAS_H
