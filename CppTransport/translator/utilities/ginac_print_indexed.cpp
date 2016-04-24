78ecdcbd9e4c0177bbdbae82d4c123b9fc39c914
ds283 <D.Seery@sussex.ac.uk>
2016-04-24 15:11:14 +0100

    Add GPL licensing statements and copyright data to all header files

--- CppTransport/translator/utilities/ginac_print_indexed.cpp
+++ CppTransport/translator/utilities/ginac_print_indexed.cpp
@@ -1,6 +1,26 @@
 //
 // Created by David Seery on 23/12/2015.
+// --@@
 // Copyright (c) 2016 University of Sussex. All rights reserved.
+//
+// This file is part of the CppTransport platform.
+//
+// CppTransport is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// CppTransport is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CppTransport.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @license: GPL-2
+// @contributor: David Seery <D.Seery@sussex.ac.uk>
+// --@@
 //
 
 #include "ginac_print_indexed.h"
