1b93d00f6daa34697878eb6112a0f5b28fa8ea6f
ds283 <D.Seery@sussex.ac.uk>
2016-05-05 09:27:56 +0100

    Adjust format_number not to use scientific notation if the number is exactly zero

--- CppTransport/transport-runtime/utilities/formatter.h
+++ CppTransport/transport-runtime/utilities/formatter.h
@@ -96,7 +96,11 @@ inline std::string format_number(double number, unsigned int precision=3)
   {
     std::ostringstream out;
 
-    if(std::abs(number) > 1E3 || std::abs(number) < 1E-3)
+    double abs_number = std::abs(number);
+
+    // use scientific notation for large or small numbers, unless
+    // the number is exactly zero
+    if(abs_number != 0.0 && (std::abs(number) > 1E3 || std::abs(number) < 1E-3))
       {
         out << std::scientific;
         if(precision > 0) --precision;    // in scientific format, precision means number of decimal digits
