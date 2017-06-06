64d72e8cad60a1066783091ae3954d30b827ae26
ds283 <D.Seery@sussex.ac.uk>
2017-06-06 16:29:59 +0100

    Implement very basic index_literal class

--- CppTransport/translator/backends/infrastructure/macro_substitution/index_literal.h
+++ CppTransport/translator/backends/infrastructure/macro_substitution/index_literal.h
@@ -28,13 +28,71 @@
 #define CPPTRANSPORT_ABSTRACT_INDEX_INSTANCE_H
 
 
+#include <memory>
+#include <list>
+
 #include "abstract_index.h"
 
 
+//! enumeration representing variance type (co-, contra-, or unset)
+enum class variance { covariant, contravariant, none };
+
+
+//! index literal represents an instance of an abstract index, and carries context information
+//! such as the variance type
 class index_literal
   {
 
+    // CONSTRUCTOR, DESTRUCTOR
+
+  public:
+
+    //! constructor; defaults to unset variance
+    index_literal(abstract_index_list::iterator& i, variance t = variance::none)
+      : idx(i),
+        type(t)
+      {
+      }
+
+    //! destructor is default
+    ~index_literal() = default;
+
+
+    // INTERFACE
+
+  public:
+
+    //! get underlying an abstract_index object
+    abstract_index& get() { return *this->idx; }
+
+    //! get underlying an abstract_index object (const version)
+    const abstract_index& get() const { abstract_index_list::const_iterator t = this->idx; return *t; }
+
+    //! allow conversion to abstract_index
+    operator abstract_index() { return this->get(); }
+
+    //! allow conversion to abstract index (const version)
+    operator const abstract_index() { return this->get(); }
+
+    //! get index variance
+    variance get_variance() const { return this->type; }
+
+
+    // INTERNAL DATA
+
+  private:
+
+    //! link to abstract index of which this is an instance
+    abstract_index_list::iterator idx;
+
+    //! variance
+    variance type;
+
   };
+
+
+//! typedef for list of index literals
+typedef std::list< std::shared_ptr<index_literal> > index_literal_list;
 
 
 #endif //CPPTRANSPORT_ABSTRACT_INDEX_INSTANCE_H
