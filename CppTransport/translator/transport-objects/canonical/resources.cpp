ds283/CppTransport

commit dc7dba58109e21e1a0fb1820ddddca46ccbcf6fb

Distinguish between field labels and field component vectors
- previously, we did not distinguish between the physical labels (phi, __dphi, …) for field-space coordinates and their labels. Obtaining a list of the field-space coordinates was handled by shared_resources and simply involved handing back the raw list of labels

- this won’t work with a nontrivial metric, because we might want the co- or contravariant components. This is not something that can be handled by shared_resources, which is supposed to be the bottom layer of resource management and is model agnostic.

- to handle this, separate the APIs for getting resource *labels* (which belong in shared_resources) from the APIs for getting a vector of coordinates (which belong in the appropriate model-dependent resource manager). This will ease the conversion to a nontrivial metric.

- further, for a nontrivial metric, a component of a coordinate vector might be a complex expression if it has to be generated by index raising or lowering. Therefore the return type of all these APIs changes from GiNaC::symbol to GiNaC::ex

CppTransport/translator/transport-objects/canonical/resources.cpp
@@ -75,6 +75,70 @@ namespace canonical
       }
 
 
+    std::unique_ptr<flattened_tensor> resources::generate_field_vector(const language_printer& printer) const
+      {
+        // no distinction between co- and contravariant indices in a flat model, so we can always return the
+        // raw variable labels (which notionally are the contravariant components)
+
+        auto X = std::make_unique<flattened_tensor>();
+
+        const auto Y = this->share.generate_field_symbols(printer);
+        for(const auto& label : *Y)
+          {
+            X->push_back(label);
+          }
+
+        return X;
+      }
+
+
+    std::unique_ptr<flattened_tensor> resources::generate_deriv_vector(const language_printer& printer) const
+      {
+        // no distinction between co- and contravariant indices in a flat model, so we can always return the
+        // raw variable labels (which notionally are the contravariant components)
+
+        auto X = std::make_unique<flattened_tensor>();
+
+        const auto Y = this->share.generate_field_symbols(printer);
+        for(const auto& label : *Y)
+          {
+            X->push_back(label);
+          }
+
+        return X;
+      }
+
+
+    GiNaC::ex resources::generate_field_vector(const abstract_index& idx, const language_printer& printer) const
+      {
+        // no distinction between co- and contravariant indices in a flat model, so we can always return the
+        // raw variable labels (which notionally are the contravariant components)
+
+        const auto resource = this->mgr.coordinates();
+        const auto& flatten = this->mgr.phase_flatten();
+
+        if(!resource || !flatten) throw resource_failure(idx.get_loop_variable());
+
+        std::string variable = printer.array_subscript(resource.get().second, idx, *flatten);
+        return this->sym_factory.get_symbol(variable);
+      }
+
+
+    GiNaC::ex resources::generate_deriv_vector(const abstract_index& idx, const language_printer& printer) const
+      {
+        // no distinction between co- and contravariant indices in a flat model, so we can always return the
+        // raw variable labels (which notionally are the contravariant components)
+
+        const auto resource = this->mgr.coordinates();
+        const auto& flatten = this->mgr.phase_flatten();
+
+        if(!resource || !flatten) throw resource_failure(idx.get_loop_variable());
+
+        std::string variable = printer.array_subscript(resource.get().second, idx, *flatten, static_cast<unsigned int>(this->num_fields));
+        return this->sym_factory.get_symbol(variable);
+      }
+
+
     GiNaC::ex resources::V_resource(cse& cse_worker, const language_printer& printer)
       {
         // behaviour differs depending whether CSE is in use or not
@@ -510,7 +574,7 @@ namespace canonical
       }
 
 
-    GiNaC::symbol resources::dV_resource(const abstract_index& a, const language_printer& printer)
+    GiNaC::ex resources::dV_resource(const abstract_index& a, const language_printer& printer)
       {
         const auto resource = this->mgr.dV();
         const auto& flatten = this->mgr.field_flatten();
@@ -522,7 +586,7 @@ namespace canonical
       }
 
 
-    GiNaC::symbol resources::ddV_resource(const abstract_index& a, const abstract_index& b, const language_printer& printer)
+    GiNaC::ex resources::ddV_resource(const abstract_index& a, const abstract_index& b, const language_printer& printer)
       {
         const auto resource = this->mgr.ddV();
         const auto& flatten = this->mgr.field_flatten();
@@ -534,8 +598,8 @@ namespace canonical
       }
 
 
-    GiNaC::symbol resources::dddV_resource(const abstract_index& a, const abstract_index& b, const abstract_index& c,
-                                           const language_printer& printer)
+    GiNaC::ex resources::dddV_resource(const abstract_index& a, const abstract_index& b, const abstract_index& c,
+                                       const language_printer& printer)
       {
         const auto resource = this->mgr.dddV();
         const auto& flatten = this->mgr.field_flatten();

CppTransport/translator/transport-objects/canonical/A.cpp
@@ -78,23 +78,23 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol& deriv_i = (*derivs)[this->fl.flatten(i)];
-            GiNaC::symbol& deriv_j = (*derivs)[this->fl.flatten(j)];
-            GiNaC::symbol& deriv_k = (*derivs)[this->fl.flatten(k)];
+            auto& deriv_i = (*derivs)[this->fl.flatten(i)];
+            auto& deriv_j = (*derivs)[this->fl.flatten(j)];
+            auto& deriv_k = (*derivs)[this->fl.flatten(k)];
 
-            GiNaC::ex& Vijk = (*dddV)[this->fl.flatten(i,j,k)];
+            auto& Vijk = (*dddV)[this->fl.flatten(i,j,k)];
 
-            GiNaC::ex& Vij  = (*ddV)[this->fl.flatten(i,j)];
-            GiNaC::ex& Vjk  = (*ddV)[this->fl.flatten(j,k)];
-            GiNaC::ex& Vik  = (*ddV)[this->fl.flatten(i,k)];
+            auto& Vij  = (*ddV)[this->fl.flatten(i,j)];
+            auto& Vjk  = (*ddV)[this->fl.flatten(j,k)];
+            auto& Vik  = (*ddV)[this->fl.flatten(i,k)];
 
-            GiNaC::ex& Vi   = (*dV)[this->fl.flatten(i)];
-            GiNaC::ex& Vj   = (*dV)[this->fl.flatten(j)];
-            GiNaC::ex& Vk   = (*dV)[this->fl.flatten(k)];
+            auto& Vi   = (*dV)[this->fl.flatten(i)];
+            auto& Vj   = (*dV)[this->fl.flatten(j)];
+            auto& Vk   = (*dV)[this->fl.flatten(k)];
 
-            GiNaC::idx idx_i = this->shared.generate_index(i);
-            GiNaC::idx idx_j = this->shared.generate_index(j);
-            GiNaC::idx idx_k = this->shared.generate_index(k);
+            auto idx_i = this->shared.generate_index(i);
+            auto idx_j = this->shared.generate_index(j);
+            auto idx_k = this->shared.generate_index(k);
 
             result = this->expr(idx_i, idx_j, idx_k, Vijk, Vij, Vjk, Vik, Vi, Vj, Vk,
                                 deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
@@ -109,7 +109,7 @@ namespace canonical
     GiNaC::ex canonical_A::expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                                 GiNaC::ex& Vijk, GiNaC::ex& Vij, GiNaC::ex& Vjk, GiNaC::ex& Vik,
                                 GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                                GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                                GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                                 GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a)
       {
         GiNaC::ex xi_i = -2*(3-eps) * deriv_i - 2 * Vi/Hsq;
@@ -186,19 +186,19 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol deriv_i = this->shared.generate_deriv_vector(i, this->printer);
-            GiNaC::symbol deriv_j = this->shared.generate_deriv_vector(j, this->printer);
-            GiNaC::symbol deriv_k = this->shared.generate_deriv_vector(k, this->printer);
+            auto deriv_i = this->res.generate_deriv_vector(i, this->printer);
+            auto deriv_j = this->res.generate_deriv_vector(j, this->printer);
+            auto deriv_k = this->res.generate_deriv_vector(k, this->printer);
 
-            GiNaC::ex Vijk = this->res.dddV_resource(i, j, k, this->printer);
+            auto Vijk = this->res.dddV_resource(i, j, k, this->printer);
 
-            GiNaC::ex Vij  = this->res.ddV_resource(i, j, this->printer);
-            GiNaC::ex Vjk  = this->res.ddV_resource(j, k, this->printer);
-            GiNaC::ex Vik  = this->res.ddV_resource(i, k, this->printer);
+            auto Vij  = this->res.ddV_resource(i, j, this->printer);
+            auto Vjk  = this->res.ddV_resource(j, k, this->printer);
+            auto Vik  = this->res.ddV_resource(i, k, this->printer);
 
-            GiNaC::ex Vi   = this->res.dV_resource(i, this->printer);
-            GiNaC::ex Vj   = this->res.dV_resource(j, this->printer);
-            GiNaC::ex Vk   = this->res.dV_resource(k, this->printer);
+            auto Vi   = this->res.dV_resource(i, this->printer);
+            auto Vj   = this->res.dV_resource(j, this->printer);
+            auto Vk   = this->res.dV_resource(k, this->printer);
 
             result = this->expr(idx_i, idx_j, idx_k, Vijk, Vij, Vjk, Vik, Vi, Vj, Vk,
                                 deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
@@ -216,7 +216,7 @@ namespace canonical
         if(cached) throw tensor_exception("A already cached");
 
         this->pre_lambda();
-        derivs = this->shared.generate_deriv_symbols(this->printer);
+        derivs = this->res.generate_deriv_vector(this->printer);
         dV = this->res.dV_resource(this->printer);
         ddV = this->res.ddV_resource(this->printer);
         dddV = this->res.dddV_resource(this->printer);

CppTransport/translator/transport-objects/canonical/A.h
@@ -109,7 +109,7 @@ namespace canonical
         GiNaC::ex expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                        GiNaC::ex& Vijk, GiNaC::ex& Vij, GiNaC::ex& Vjk, GiNaC::ex& Vik,
                        GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                       GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                       GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                        GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a);
 
 
@@ -154,7 +154,7 @@ namespace canonical
         // WORKSPACE AND CACHE
 
         //! list of momentum symbols
-        std::unique_ptr<symbol_list> derivs;
+        std::unique_ptr<flattened_tensor> derivs;
 
         //! flattened dV tensor
         std::unique_ptr<flattened_tensor> dV;

CppTransport/translator/transport-objects/canonical/Atilde.cpp
@@ -78,23 +78,23 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol& deriv_i = (*derivs)[this->fl.flatten(i)];
-            GiNaC::symbol& deriv_j = (*derivs)[this->fl.flatten(j)];
-            GiNaC::symbol& deriv_k = (*derivs)[this->fl.flatten(k)];
+            auto& deriv_i = (*derivs)[this->fl.flatten(i)];
+            auto& deriv_j = (*derivs)[this->fl.flatten(j)];
+            auto& deriv_k = (*derivs)[this->fl.flatten(k)];
 
-            GiNaC::ex& Vijk = (*dddV)[this->fl.flatten(i,j,k)];
+            auto& Vijk = (*dddV)[this->fl.flatten(i,j,k)];
 
-            GiNaC::ex& Vij  = (*ddV)[this->fl.flatten(i,j)];
-            GiNaC::ex& Vjk  = (*ddV)[this->fl.flatten(j,k)];
-            GiNaC::ex& Vik  = (*ddV)[this->fl.flatten(i,k)];
+            auto& Vij  = (*ddV)[this->fl.flatten(i,j)];
+            auto& Vjk  = (*ddV)[this->fl.flatten(j,k)];
+            auto& Vik  = (*ddV)[this->fl.flatten(i,k)];
 
-            GiNaC::ex& Vi   = (*dV)[this->fl.flatten(i)];
-            GiNaC::ex& Vj   = (*dV)[this->fl.flatten(j)];
-            GiNaC::ex& Vk   = (*dV)[this->fl.flatten(k)];
+            auto& Vi   = (*dV)[this->fl.flatten(i)];
+            auto& Vj   = (*dV)[this->fl.flatten(j)];
+            auto& Vk   = (*dV)[this->fl.flatten(k)];
 
-            GiNaC::idx idx_i = this->shared.generate_index(i);
-            GiNaC::idx idx_j = this->shared.generate_index(j);
-            GiNaC::idx idx_k = this->shared.generate_index(k);
+            auto idx_i = this->shared.generate_index(i);
+            auto idx_j = this->shared.generate_index(j);
+            auto idx_k = this->shared.generate_index(k);
 
             result = this->expr(idx_i, idx_j, idx_k, Vijk, Vij, Vjk, Vik, Vi, Vj, Vk,
                                 deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
@@ -109,7 +109,7 @@ namespace canonical
     GiNaC::ex canonical_Atilde::expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                                      GiNaC::ex& Vijk, GiNaC::ex& Vij, GiNaC::ex& Vjk, GiNaC::ex& Vik,
                                      GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                                     GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                                     GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                                      GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a)
       {
         GiNaC::ex xi_i = -2*(3-eps) * deriv_i - 2 * Vi/Hsq;
@@ -178,19 +178,19 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol deriv_i = this->shared.generate_deriv_vector(i, this->printer);
-            GiNaC::symbol deriv_j = this->shared.generate_deriv_vector(j, this->printer);
-            GiNaC::symbol deriv_k = this->shared.generate_deriv_vector(k, this->printer);
+            auto deriv_i = this->res.generate_deriv_vector(i, this->printer);
+            auto deriv_j = this->res.generate_deriv_vector(j, this->printer);
+            auto deriv_k = this->res.generate_deriv_vector(k, this->printer);
 
-            GiNaC::ex Vijk = this->res.dddV_resource(i, j, k, this->printer);
+            auto Vijk = this->res.dddV_resource(i, j, k, this->printer);
 
-            GiNaC::ex Vij  = this->res.ddV_resource(i, j, this->printer);
-            GiNaC::ex Vjk  = this->res.ddV_resource(j, k, this->printer);
-            GiNaC::ex Vik  = this->res.ddV_resource(i, k, this->printer);
+            auto Vij  = this->res.ddV_resource(i, j, this->printer);
+            auto Vjk  = this->res.ddV_resource(j, k, this->printer);
+            auto Vik  = this->res.ddV_resource(i, k, this->printer);
 
-            GiNaC::ex Vi   = this->res.dV_resource(i, this->printer);
-            GiNaC::ex Vj   = this->res.dV_resource(j, this->printer);
-            GiNaC::ex Vk   = this->res.dV_resource(k, this->printer);
+            auto Vi   = this->res.dV_resource(i, this->printer);
+            auto Vj   = this->res.dV_resource(j, this->printer);
+            auto Vk   = this->res.dV_resource(k, this->printer);
 
             result = this->expr(idx_i, idx_j, idx_k, Vijk, Vij, Vjk, Vik, Vi, Vj, Vk,
                                 deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
@@ -225,7 +225,7 @@ namespace canonical
         if(cached) throw tensor_exception("Atilde already cached");
 
         this->pre_lambda();
-        derivs = this->shared.generate_deriv_symbols(this->printer);
+        derivs = this->res.generate_deriv_vector(this->printer);
         dV = this->res.dV_resource(this->printer);
         ddV = this->res.ddV_resource(this->printer);
         dddV = this->res.dddV_resource(this->printer);

CppTransport/translator/transport-objects/canonical/Atilde.h
@@ -109,7 +109,7 @@ namespace canonical
         GiNaC::ex expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                        GiNaC::ex& Vijk, GiNaC::ex& Vij, GiNaC::ex& Vjk, GiNaC::ex& Vik,
                        GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                       GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                       GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                        GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a);
 
 
@@ -154,7 +154,7 @@ namespace canonical
         // WORKSPACE AND CACHE
 
         //! list of momentum symbols
-        std::unique_ptr<symbol_list> derivs;
+        std::unique_ptr<flattened_tensor> derivs;
 
         //! flattened dV tensor
         std::unique_ptr<flattened_tensor> dV;

CppTransport/translator/transport-objects/canonical/B.cpp
@@ -77,17 +77,17 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::ex& Vi   = (*dV)[this->fl.flatten(i)];
-            GiNaC::ex& Vj   = (*dV)[this->fl.flatten(j)];
-            GiNaC::ex& Vk   = (*dV)[this->fl.flatten(k)];
+            auto& Vi   = (*dV)[this->fl.flatten(i)];
+            auto& Vj   = (*dV)[this->fl.flatten(j)];
+            auto& Vk   = (*dV)[this->fl.flatten(k)];
 
-            GiNaC::symbol& deriv_i = (*derivs)[this->fl.flatten(i)];
-            GiNaC::symbol& deriv_j = (*derivs)[this->fl.flatten(j)];
-            GiNaC::symbol& deriv_k = (*derivs)[this->fl.flatten(k)];
+            auto& deriv_i = (*derivs)[this->fl.flatten(i)];
+            auto& deriv_j = (*derivs)[this->fl.flatten(j)];
+            auto& deriv_k = (*derivs)[this->fl.flatten(k)];
 
-            GiNaC::idx idx_i = this->shared.generate_index(i);
-            GiNaC::idx idx_j = this->shared.generate_index(j);
-            GiNaC::idx idx_k = this->shared.generate_index(k);
+            auto idx_i = this->shared.generate_index(i);
+            auto idx_j = this->shared.generate_index(j);
+            auto idx_k = this->shared.generate_index(k);
 
             result = this->expr(idx_i, idx_j, idx_k, Vi, Vj, Vk,
                                 deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
@@ -101,7 +101,7 @@ namespace canonical
 
     GiNaC::ex canonical_B::expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                                 GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                                GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                                GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                                 GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a)
       {
         GiNaC::ex xi_i = -2*(3-eps) * deriv_i - 2 * Vi/Hsq;
@@ -161,13 +161,13 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol deriv_i = this->shared.generate_deriv_vector(i, this->printer);
-            GiNaC::symbol deriv_j = this->shared.generate_deriv_vector(j, this->printer);
-            GiNaC::symbol deriv_k = this->shared.generate_deriv_vector(k, this->printer);
+            auto deriv_i = this->res.generate_deriv_vector(i, this->printer);
+            auto deriv_j = this->res.generate_deriv_vector(j, this->printer);
+            auto deriv_k = this->res.generate_deriv_vector(k, this->printer);
 
-            GiNaC::ex Vi   = this->res.dV_resource(i, this->printer);
-            GiNaC::ex Vj   = this->res.dV_resource(j, this->printer);
-            GiNaC::ex Vk   = this->res.dV_resource(k, this->printer);
+            auto Vi   = this->res.dV_resource(i, this->printer);
+            auto Vj   = this->res.dV_resource(j, this->printer);
+            auto Vk   = this->res.dV_resource(k, this->printer);
 
             result = this->expr(idx_i, idx_j, idx_k, Vi, Vj, Vk, deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
 
@@ -207,7 +207,7 @@ namespace canonical
         if(cached) throw tensor_exception("B already cached");
 
         this->pre_lambda();
-        derivs = this->shared.generate_deriv_symbols(this->printer);
+        derivs = this->res.generate_deriv_vector(this->printer);
         dV = this->res.dV_resource(this->printer);
 
         this->cached = true;

CppTransport/translator/transport-objects/canonical/B.h
@@ -108,7 +108,7 @@ namespace canonical
         //! underlying symbolic expression
         GiNaC::ex expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
                        GiNaC::ex& Vi, GiNaC::ex& Vj, GiNaC::ex& Vk,
-                       GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                       GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                        GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a);
 
 
@@ -153,7 +153,7 @@ namespace canonical
         // WORKSPACE AND CACHE
 
         //! list of momentum symbols
-        std::unique_ptr<symbol_list> derivs;
+        std::unique_ptr<flattened_tensor> derivs;
 
         //! flattened dV tensor
         std::unique_ptr<flattened_tensor> dV;

CppTransport/translator/transport-objects/canonical/C.cpp
@@ -77,13 +77,13 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol& deriv_i = (*derivs)[this->fl.flatten(i)];
-            GiNaC::symbol& deriv_j = (*derivs)[this->fl.flatten(j)];
-            GiNaC::symbol& deriv_k = (*derivs)[this->fl.flatten(k)];
+            auto& deriv_i = (*derivs)[this->fl.flatten(i)];
+            auto& deriv_j = (*derivs)[this->fl.flatten(j)];
+            auto& deriv_k = (*derivs)[this->fl.flatten(k)];
 
-            GiNaC::idx idx_i = this->shared.generate_index(i);
-            GiNaC::idx idx_j = this->shared.generate_index(j);
-            GiNaC::idx idx_k = this->shared.generate_index(k);
+            auto idx_i = this->shared.generate_index(i);
+            auto idx_j = this->shared.generate_index(j);
+            auto idx_k = this->shared.generate_index(k);
 
             result = this->expr(idx_i, idx_j, idx_k, deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
 
@@ -95,7 +95,7 @@ namespace canonical
 
 
     GiNaC::ex canonical_C::expr(GiNaC::idx& i, GiNaC::idx& j, GiNaC::idx& k,
-                                GiNaC::symbol& deriv_i, GiNaC::symbol& deriv_j, GiNaC::symbol& deriv_k,
+                                GiNaC::ex& deriv_i, GiNaC::ex& deriv_j, GiNaC::ex& deriv_k,
                                 GiNaC::symbol& k1, GiNaC::symbol& k2, GiNaC::symbol& k3, GiNaC::symbol& a)
       {
         GiNaC::ex k1dotk2 = (k3*k3 - k1*k1 - k2*k2) / 2;
@@ -151,9 +151,9 @@ namespace canonical
           {
             timing_instrument timer(this->compute_timer);
 
-            GiNaC::symbol deriv_i = this->shared.generate_deriv_vector(i, this->printer);
-            GiNaC::symbol deriv_j = this->shared.generate_deriv_vector(j, this->printer);
-            GiNaC::symbol deriv_k = this->shared.generate_deriv_vector(k, this->printer);
+            auto deriv_i = this->res.generate_deriv_vector(i, this->printer);
+            auto deriv_j = this->res.generate_deriv_vector(j, this->printer);
+            auto deriv_k = this->res.generate_deriv_vector(k, this->printer);
 
             result = this->expr(idx_i, idx_j, idx_k, deriv_i, deriv_j, deriv_k, k1, k2, k3, a);
 
@@ -185,7 +185,7 @@ namespace canonical
       {
         if(cached) throw tensor_exception("C already cached");
 
-        derivs = this->shared.generate_deriv_symbols(this->printer);
+        derivs = this->res.generate_deriv_vector(this->printer);
 
         this->cached = true;
       }

commit b4ecf38cbd240cfa3cecba35f29e0a4f0ede71bc

Fix typo introduced in dc7dba58

CppTransport/translator/transport-objects/canonical/resources.cpp
@@ -100,7 +100,7 @@ namespace canonical
 
         auto X = std::make_unique<flattened_tensor>();
 
-        const auto Y = this->share.generate_field_symbols(printer);
+        const auto Y = this->share.generate_deriv_symbols(printer);
         for(const auto& label : *Y)
           {
             X->push_back(label);

CppTransport/translator/transport-objects/nontrivial-metric/resources.cpp
@@ -100,7 +100,7 @@ namespace nontrivial_metric
 
         auto X = std::make_unique<flattened_tensor>();
 
-        const auto Y = this->share.generate_field_symbols(printer);
+        const auto Y = this->share.generate_deriv_symbols(printer);
         for(const auto& label : *Y)
           {
             X->push_back(label);