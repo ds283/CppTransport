17fd70fed125ac4befadc75037f5cda938746392
ds283 <D.Seery@sussex.ac.uk>
2017-07-03 17:12:09 +0100

    Update test code for nontrivial-metric model
    - revert to QSFI/Gelaton model considered in original transport paper, and add some extra diagnostic outputs

--- CppTransport/test-nontrivial-metric/test-nontrivial-metric.cpp
+++ CppTransport/test-nontrivial-metric/test-nontrivial-metric.cpp
@@ -38,72 +38,97 @@ void write_tasks(transport::repository<>& repo, transport::gelaton_mpi<>* model)
     
     const double V0            = 0.1 * P_zeta * M_P*M_P*M_P*M_P;                                // adjust uplift to get sufficient inflation
     const double eta_R         = 1.0/std::sqrt(3.0);                                            // adjust radial mass to be of order Hubble
-    const double g_R           = 115573.26157418323;                                            // magic value found by Anjelah
-    const double lambda_R      = 0.0;                                                           // don't use quartic coupling
+    const double g_R           = M_P*M_P / std::sqrt(V0);                                       // adjust radial cubic coupling to be of order Hubble
+    const double lambda_R      = 0.5 * M_P*M_P*M_P / std::pow(V0, 3.0/4.0) / std::sqrt(omega);  // adjust radial quartic coupling to dominate the displacement
     
-    const double R0            = 0.00007053497338915804 * M_P;                                  // matches magic g_R value
+    const double R0            = std::sqrt(V0/3.0) / (M_P * omega * std::sqrt(P_zeta));         // adjust radial minimum to give desired P_zeta normalization
     
-    const double R_init        = (1.0 + 1E-3) * R0;
-    const double theta_init    = M_PI - 1E-3;
+    const double x_init        = -R0;
+    const double y_init        = (1E-2)*R0;
+    const double R_init        = std::sqrt(x_init*x_init + y_init*y_init);
+    const double theta_init    = std::atan2(y_init, x_init);
     
     const double N_init        = 0.0;
     const double N_pre         = 8.0;
-    const double N_max         = 58.0;
+    const double N_max         = 28.0;
     
-    transport::parameters<> params(M_P, { R0, V0, eta_R, g_R, lambda_R, omega }, model);
-    transport::initial_conditions<> ics("gelaton", params, { R_init, theta_init, 0.0, 0.0 }, N_init, N_pre);
+    transport::parameters<> params{M_P, { R0, V0, eta_R, g_R, lambda_R, omega }, model};
+    transport::initial_conditions<> ics{"gelaton", params, { R_init, theta_init, 0.0, 0.0 }, N_init, N_pre};
     
-    transport::basic_range<> times(N_init, N_max, 500, transport::spacing::linear);
+    transport::basic_range<> times{N_init, N_max, 500, transport::spacing::linear};
     
-    transport::basic_range<> ks(exp(10.0), exp(18.5), 1000, transport::spacing::log_bottom);
-    transport::basic_range<> kts(exp(10.0), exp(17.0), 100, transport::spacing::log_bottom);
-    transport::basic_range<> alphas(0.0, 0.0, 0, transport::spacing::linear);
-    transport::basic_range<> betas(1.0/3.0, 1.0/3.0, 0, transport::spacing::linear);
+    transport::basic_range<> ks{exp(10.0), exp(10.0), 0, transport::spacing::log_bottom};
+    transport::basic_range<> kts{exp(10.0), exp(17.0), 100, transport::spacing::log_bottom};
+    transport::basic_range<> alphas{0.0, 0.0, 0, transport::spacing::linear};
+    transport::basic_range<> betas{1.0/3.0, 1.0/3.0, 0, transport::spacing::linear};
     
     // construct a twopf task
-    transport::twopf_task<> tk2("gelaton.twopf", ics, times, ks);
+    transport::twopf_task<> tk2{"gelaton.twopf", ics, times, ks};
     tk2.set_collect_initial_conditions(true).set_adaptive_ics_efolds(5.0);
     
     // construct a threepf task
-    transport::threepf_alphabeta_task<> tk3("gelaton.threepf", ics, times, kts, alphas, betas);
+    transport::threepf_alphabeta_task<> tk3{"gelaton.threepf", ics, times, kts, alphas, betas};
     tk3.set_collect_initial_conditions(true).set_adaptive_ics_efolds(5.0);
     
-    transport::zeta_twopf_task<> ztk2("gelaton.twopf-zeta", tk2);
-    transport::zeta_threepf_task<> ztk3("gelaton.threepf-zeta", tk3);
+    transport::zeta_twopf_task<> ztk2{"gelaton.twopf-zeta", tk2};
+    transport::zeta_threepf_task<> ztk3{"gelaton.threepf-zeta", tk3};
     
     ztk2.set_paired(true);
     ztk3.set_paired(true);
     
-    vis_toolkit::SQL_time_query tquery("1=1");
-    vis_toolkit::SQL_twopf_query k2query("comoving IN (SELECT MAX(comoving) FROM twopf_samples UNION SELECT MIN(comoving) FROM twopf_samples)");
-    vis_toolkit::SQL_threepf_query k3query("kt_comoving IN (SELECT MAX(kt_comoving) FROM threepf_samples UNION SELECT MIN(kt_comoving) FROM threepf_samples)");
+    vis_toolkit::SQL_time_query tquery{"1=1"};
+    vis_toolkit::SQL_twopf_query k2query{"comoving IN (SELECT MAX(comoving) FROM twopf_samples UNION SELECT MIN(comoving) FROM twopf_samples)"};
+    vis_toolkit::SQL_threepf_query k3query{"kt_comoving IN (SELECT MAX(kt_comoving) FROM threepf_samples UNION SELECT MIN(kt_comoving) FROM threepf_samples)"};
     
-    vis_toolkit::zeta_twopf_time_series<> z2pf(ztk3, tquery, k2query);    // defaults to dimensionless
-    vis_toolkit::largest_u2_line<> u2line(tk3, tquery, k2query);
-    vis_toolkit::largest_u3_line<> u3line(tk3, tquery, k3query);
-    vis_toolkit::background_line<> hubble(tk3, tquery, vis_toolkit::background_quantity::Hubble);
+    vis_toolkit::zeta_twopf_time_series<> z2pf{ztk3, tquery, k2query};    // defaults to dimensionless
+    vis_toolkit::largest_u2_line<> u2line{tk3, tquery, k2query};
+    vis_toolkit::largest_u3_line<> u3line{tk3, tquery, k3query};
+    vis_toolkit::background_line<> hubble{tk3, tquery, vis_toolkit::background_quantity::Hubble};
     
-    vis_toolkit::time_series_table<> table("gelaton.utable", "utable.csv");
+    vis_toolkit::index_selector<2> f2_fields{model->get_N_fields()};
+    f2_fields.all();
+//    f2_fields.none().set_on({0,0}).set_on({0,1}).set_on({1,1});
+    vis_toolkit::index_selector<2> f2_momenta{model->get_N_fields()};
+    f2_momenta.none().set_on({2,2}).set_on({2,3}).set_on({3,3});
+    
+    vis_toolkit::twopf_time_series<> tk2_fields{tk2, f2_fields, tquery, k2query};
+    vis_toolkit::twopf_time_series<> tk2_momenta{tk2, f2_momenta, tquery, k2query};
+    
+    vis_toolkit::time_series_plot<> tk2_fplot{"gelaton.twopf-1.fields-plot", "fields-plot.pdf"};
+    tk2_fplot += tk2_fields;
+    vis_toolkit::time_series_plot<> tk2_mplot{"gelaton.twopf-1.momenta-plot", "momenta-plot.pdf"};
+    tk2_mplot += tk2_momenta;
+    
+    transport::output_task<> tk2_out{"gelaton.test"};
+    tk2_out += tk2_fplot + tk2_mplot;
+    repo.commit(tk2_out);
+    
+    vis_toolkit::time_series_table<> table{"gelaton.utable", "utable.csv"};
     table += z2pf + u2line + u3line + hubble;
     
-    vis_toolkit::index_selector<3> fsel(model->get_N_fields());
+    vis_toolkit::index_selector<3> fsel{model->get_N_fields()};
     fsel.none().set_on({0,0,0}).set_on({1,1,1});
-    vis_toolkit::threepf_time_series<> tpf(tk3, fsel, tquery, k3query);
+    vis_toolkit::threepf_time_series<> tpf{tk3, fsel, tquery, k3query};
     
-    vis_toolkit::time_series_plot<> plot("gelaton.threepf-fields", "threepf-fields.pdf");
+    vis_toolkit::time_series_plot<> plot{"gelaton.threepf-fields", "threepf-fields.pdf"};
     plot += tpf;
     
-    vis_toolkit::SQL_time_query tquery2("serial IN (SELECT MAX(serial) FROM time_samples)");
+    vis_toolkit::zeta_threepf_time_series<> ztpf{ztk3, tquery, k3query};
+    
+    vis_toolkit::time_series_plot<> plot2{"gelaton.threepf-zeta", "threepf-zeta.pdf"};
+    plot2 += ztpf;
+    
+    vis_toolkit::SQL_time_query tquery2{"serial IN (SELECT MAX(serial) FROM time_samples)"};
     vis_toolkit::SQL_threepf_query k3query2("1=1");
     
-    vis_toolkit::zeta_reduced_bispectrum_wavenumber_series<> fNL(ztk3, tquery2, k3query2);
+    vis_toolkit::zeta_reduced_bispectrum_wavenumber_series<> fNL{ztk3, tquery2, k3query2};
+    fNL.set_current_x_axis_value(vis_toolkit::axis_value::efolds_exit);
     
-    vis_toolkit::wavenumber_series_plot<> fNLplot("gelaton.fNL", "fNL.pdf");
-    fNLplot.set_log_x(true);
+    vis_toolkit::wavenumber_series_plot<> fNLplot{"gelaton.fNL", "fNL.pdf"};
     fNLplot += fNL;
     
-    transport::output_task<> otk("gelaton.output");
-    otk += table + plot + fNLplot;
+    transport::output_task<> otk{"gelaton.output"};
+    otk += table + plot + plot2 + fNLplot;
     
     repo.commit(ztk2);
     repo.commit(ztk3);
