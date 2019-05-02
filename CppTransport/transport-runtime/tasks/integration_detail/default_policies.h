0168b99614c5993530c97a43bc455d9537f26a47
ds283 <D.Seery@sussex.ac.uk>
2019-05-02 15:59:52 +0100

    Move condition k1 > k2 > k3 into triangle StoragePolicy object rather than TrianglePolicy object
    - currently it is the task of the TrianglePolicy to impose the condition k1  > k2 > k3,  but conceptually this isn’t right: whether or not we keep k1 > k2 > k3 or some other arrangement of ks is a storage decision, not a decision on whether { k1, k2, k3 } can form a triangle. Therefore, conceptually, the decision should be taken in StoragePolicy

--- CppTransport/transport-runtime/tasks/integration_detail/default_policies.h
+++ CppTransport/transport-runtime/tasks/integration_detail/default_policies.h
@@ -38,7 +38,7 @@ namespace transport
     namespace task_policy_impl
       {
 
-        // default storage policy stores all triangles
+        // default storage policy stores all triangles that satisfy k1 > k2 > k3
         class DefaultStoragePolicy
           {
 
@@ -47,7 +47,10 @@ namespace transport
           public:
 
             //! constructor is default
-            DefaultStoragePolicy() = default;
+            DefaultStoragePolicy(double tol = CPPTRANSPORT_DEFAULT_KCONFIG_TOLERANCE)
+              : tolerance(std::abs(tol))
+              {
+              }
 
             //! destructor is default
             ~DefaultStoragePolicy() = default;
@@ -59,8 +62,23 @@ namespace transport
 
             enum storage_outcome operator()(const threepf_kconfig& data)
               {
+                // impose k1 > k2 > k3
+                if(data.k2_conventional - data.k1_conventional > this->tolerance)
+                  return storage_outcome::reject_remove;
+
+                if(data.k3_conventional - data.k2_conventional > this->tolerance)
+                  return storage_outcome::reject_remove;
+
                 return(storage_outcome::accept);
               }
+
+
+            // INTERNAL DATA
+
+          private:
+
+            //! tolerance
+            double tolerance;
 
           };
 
@@ -89,13 +107,9 @@ namespace transport
 
             bool operator()(unsigned int i, unsigned int j, unsigned int k, double k1, double k2, double k3)
               {
-                // impose ordering k1 > k2 > k3
-                if(i < j) return false;
-                if(j < k) return false;
-
                 // impose triangle
                 double max = std::max(std::max(k1, k2), k3);
-                return(k1 + k2 + k3 - 2.0*max >= -tolerance);
+                return(k1 + k2 + k3 - 2.0*max >= -this->tolerance);
               }
 
 
@@ -139,17 +153,13 @@ namespace transport
                 if(beta > 1.0) return false;
 
                 // alpha should lie between 1-beta and beta-1
-                if(beta - 1.0 - alpha > tolerance) return false;
-                if(alpha - (1.0 - beta) > tolerance) return false;
-
-                // impose k1 > k2 > k3
-                if(alpha < 0) return false;
-                if(beta - (1.0 + alpha)/3.0 < -tolerance) return false;
+                if(beta - 1.0 - alpha > this->tolerance) return false;
+                if(alpha - (1.0 - beta) > this->tolerance) return false;
 
                 // demand that squeezing should not be too small
-                if(std::abs(1.0 - beta) < smallest_squeezing) return false;
-                if(std::abs(1.0 + alpha + beta) < smallest_squeezing) return false;
-                if(std::abs(1.0 - alpha + beta) < smallest_squeezing) return false;
+                if(std::abs(1.0 - beta) < this->smallest_squeezing) return false;
+                if(std::abs(1.0 + alpha + beta) < this->smallest_squeezing) return false;
+                if(std::abs(1.0 - alpha + beta) < this->smallest_squeezing) return false;
 
                 return true;
               }
